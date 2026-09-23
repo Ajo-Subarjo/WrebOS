5f20ebd9f45881113a6fc2dc7fb362a209d13d38
Ajo-Subarjo <ajosubarjo02@gmail.com>
2026-09-23 08:03:34 +0700

    fixing background issue in build deploy

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,12 +1,12 @@
 import { apps } from "../registry/apps"
 import { useOSStore } from "../store/os"
+import wpp from "../assets/wallpaper.jpg"
 
 
 export default function Desktop() {
   const openApp = useOSStore((state) => state.openApp)
-
   return (
-    <div className="desktop">
+    <div className="desktop" style={{ backgroundImage: `url(${wpp})`}}>
       {apps.map((app) => (
         <div key={app.id} className="desktop-icon" onDoubleClick={() => openApp(app.id, app.name)}>
           <div className="desktop-icon-image">
